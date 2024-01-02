3ba49b422e95a1f72984ec9ea10e2c4833c6262b
adamlieske <119051444+adamlieske@users.noreply.github.com>
2024-01-02 23:08:09 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import { gameRoutes } from "./routes/game.js";
+
+export const app = express();
+
+app.listen(3000, () => {
+  console.log(
+    "Server is listening a http://localhost:3000/ Let 's play a game"
+  );
+});
+
+const publicPath = path.join(
+  path.dirname(fileURLToPath(import.meta.url)),
+  "public"
+);
+app.use(express.static(publicPath));
+
+gameRoutes(app);
